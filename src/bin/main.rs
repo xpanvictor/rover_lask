0474e2b26214623378f525d125f7fdcd0650408a
xpanvictor <xpanvictor@gmail.com>
2026-07-21 13:04:46 +0100

    feat: basic setup

---
+++ src/bin/main.rs
@@ -0,0 +1,159 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use core::pin::Pin;
+use core::sync::atomic::AtomicU32;
+use core::task::Poll;
+
+use defmt::{error, info};
+use embassy_executor::Spawner;
+use embassy_executor::raw::Executor;
+use embassy_time::{Duration, Instant, Ticker, Timer};
+use esp_hal::clock::CpuClock;
+use esp_hal::system::{CpuControl, Stack};
+use esp_hal::timer::timg::TimerGroup;
+use esp_println as _;
+use static_cell::StaticCell;
+
+#[panic_handler]
+fn panic(panic_info: &core::panic::PanicInfo) -> ! {
+    error!("{}", panic_info);
+    loop {}
+}
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+// core2
+static CORE1_STACK: StaticCell<Stack<65536>> = StaticCell::new();
+static EXECUTOR_C1: StaticCell<Executor> = StaticCell::new();
+
+static WHEEL_SPEED: AtomicU32 = AtomicU32::new(0);
+
+#[derive(Clone, Copy)]
+struct VehicleState {
+    speed_mm_s: u32,
+    steer: i16,
+    motors_on: bool,
+}
+
+#[derive(Clone, Copy)]
+struct Command {
+    steer: i16,
+    throttle: i16,
+}
+
+struct DelayMS {
+    ms: u64,
+    inner: Option<Timer>,
+    deadline: Option<Instant>,
+}
+
+impl DelayMS {
+    pub fn new(ms: u64) -> Self {
+        Self {
+            ms,
+            inner: None,
+            deadline: None,
+        }
+    }
+}
+
+impl Future for DelayMS {
+    type Output = ();
+    fn poll(
+        mut self: core::pin::Pin<&mut Self>,
+        cx: &mut core::task::Context<'_>,
+    ) -> core::task::Poll<Self::Output> {
+        if self.deadline.is_none() {
+            let deadline = Instant::now() + Duration::from_millis(self.ms);
+            self.deadline = Some(deadline);
+            self.inner = Some(Timer::at(deadline));
+        }
+
+        if self.deadline.is_some_and(|d| d <= Instant::now()) {
+            return Poll::Ready(());
+        }
+
+        // poll cx
+        let inner = self.inner.as_mut().unwrap();
+        let inner = unsafe { Pin::new_unchecked(inner) };
+        match inner.poll(cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(()) => Poll::Ready(()),
+        }
+    }
+}
+
+macro_rules! mk_static {
+    ($t:ty, $val:expr) => {{
+        static CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        CELL.uninit().write($val)
+    }};
+}
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
+    // generator version: 1.3.0
+    // generator parameters: --chip esp32 -o unstable-hal -o embassy -o defmt -o neovim -o vscode
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let sw_interrupt =
+        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+
+    info!("Embassy initialized!");
+
+    // Access core2
+    let mut cpu_ctrl = CpuControl::new(peripherals.CPU_CTRL);
+    let stack = CORE1_STACK.init(Stack::new());
+    let c1_executor = EXECUTOR_C1.init();
+
+    let _guard = cpu_ctrl.start_app_core(stack, move || {});
+
+    // TODO: Spawn some tasks
+    spawner.spawn(speed_sensor().unwrap());
+    spawner.spawn(control_loop().unwrap());
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
+}
+
+#[embassy_executor::task]
+async fn control_loop() {
+    let mut ticker = Ticker::every(Duration::from_millis(50));
+    loop {
+        let speed = WHEEL_SPEED.load(core::sync::atomic::Ordering::Relaxed);
+        defmt::info!("speed: {}", speed);
+        ticker.next().await
+    }
+}
+
+#[embassy_executor::task]
+async fn speed_sensor() {
+    let mut counter = 0;
+    loop {
+        DelayMS::new(20).await;
+        WHEEL_SPEED.store(counter, core::sync::atomic::Ordering::Relaxed);
+        counter += 1;
+        embassy_futures::yield_now().await;
+    }
+}
